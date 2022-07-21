6ac9143cd38f9b9848ccb336c229007ed84a03e3
Jacob Manley <jbmanley45@gmail.com>
2022-07-21 13:46:48 -0700

    new-build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import RecipeList  from './components/RecipeList';
+import RecipeCard from './components/RecipeCard';
+import RecipeForm from './components/RecipeForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    
+      <Routes>
+        
+        <Route exact path="/" component={<Home />}/>
+        <Route path="/login" component={<Login />}/>
+        <Route path="/signup" component={ <SignUp />}/>
+        <Route path="/recipelist" component={<RecipeList />}/>
+        <Route path="/recipecard" component={<RecipeCard />}/>
+        <Route path="/recipe/new" component={ <RecipeForm />}/> 
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Home = () => {
+  return (
+    <div>
+    <div>Cocktail Hour</div>
+    <p>Instructions: Use ingredient list to build your own cocktail.  </p>
+    </div>
+  )
+}
+export default Home;
