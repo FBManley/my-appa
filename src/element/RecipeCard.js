86b94635284f1e8d93037cd5ea348221065e2b89
Jacob Manley <jbmanley45@gmail.com>
2022-07-27 19:07:33 -0700

    added card element

--- src/element/RecipeCard.js
+++ src/element/RecipeCard.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const RecipeCard = () => {
+const RecipeCard = ({ recipes }) => { //send props from recipeLists, render a card for single db.json objectn by id
+    console.log(recipes)
     
+
   return (
-    <div>RecipeCard</div>
+
+    <div>RecipeCard
+        <h2 recipes>{[0].props.value.name}</h2>
+        {/* <h2>{amount}</h2>
+        <h2>{ingredients}</h2>
+        <h2>{instructions}</h2>
+        <h2>{id}</h2> */}
+      name: '',
+      amount: '',
+      ingredients: '',
+      instructions: ''
+    </div>
   )
 }
-export default RecipeCard;
+export default RecipeCard
+//[0].props.value.id
+//destructuring takes the KEYS from the PROPS OBJECT and creates VARIABLES with each same names
--- src/element/RecipeList.js
+++ src/element/RecipeList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react'
 import NewRecipeButton from "./NewRecipeButton";
-
+import RecipeCard from "./RecipeCard";
+// import RecipeCard from "./RecipeCard";
 //on startup (empty square brackets), sends GET req to db.json and renders saved recipes on the page. 
 
 const RecipeList = () => {
@@ -12,17 +13,22 @@ const RecipeList = () => {
     .then(data => setRecipes(data))
   }, [])
 
-  const recipesList = recipes.map((r => <li key={r.id} value={r}>{r.name}</li>)) //make able to delete
+ const recipesList = recipes.map((r => <li key={r.id} value={r}>{r.name}{r.amount}{r.ingredients}{r.instructions}</li>)) //need to render a single RecipeCard component for each li
+
   // const deleterecipe = () => {}
   return (
     <div>
+      
       <h3>My recipe list:</h3>
       <hr />
-      {recipesList}
+      {recipes}
       <hr/>
       <NewRecipeButton />
+      <RecipeCard recipes={recipesList}/>
       </div>
   )
 }
-
+// 
 export default RecipeList;
+
+// functional components can have only one prop passed to them, according to the REACT documentation: meaning i have to map in the parent before sending to child
